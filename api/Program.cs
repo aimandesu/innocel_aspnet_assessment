a61128510522566c78d866dfa7a9580b58650d81
aimandesu <aimanafiqbinesam@gmail.com>
2025-05-30 18:05:27 +0800

    initialisation cleanup setup

---
+++ api/Program.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
